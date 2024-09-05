a5cf053ab3144f7e04aa5e046419257c9a14b306
Ahmed Sayed <alngm.8765@gmail.com>
2024-09-05 10:45:02 +0300

    fitst commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// Create Burger Icon Menu
+let burgerIcon = document.querySelector(".burger-menu .icon");
+let burgerMenu = document.querySelector(".burger-menu .burger");
+let linksBurgerMenu = document.querySelectorAll(".burger ul li");
+
+burgerIcon.addEventListener("click", () => {
+    if (burgerMenu.classList.contains("active")) {
+        burgerIcon.classList.remove("active");
+        burgerMenu.classList.remove("active");
+    } else {
+        burgerIcon.classList.add("active");
+        burgerMenu.classList.add("active");
+    }
+})
+
+linksBurgerMenu.forEach((el) => {
+    el.addEventListener("click", () => {
+        burgerIcon.classList.remove("active");
+        burgerMenu.classList.remove("active");
+    })
+});
